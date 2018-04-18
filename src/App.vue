3c606e354d4fa7057a4d646a18b6cb1676e96abe
Evergreen <vincentwang.720@gmail.com>
2018-04-18 14:05:49 +0800

    feat(Button): button 组件✅

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,38 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/elements/button">Button</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+body {
+  background-color: #f5f5f9;
+}
 #app {
+  padding: 10px;
+
+  text-align: center;
+
+  color: #2c3e50;
+
   font-family: "Avenir", Helvetica, Arial, sans-serif;
+
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
+.home {
+   padding: 10px 0;
+ }
 #nav {
   padding: 30px;
   a {
-    font-weight: bold;
     color: #2c3e50;
+
+    font-weight: bold;
     &.router-link-exact-active {
       color: #42b983;
     }
---
+++ src/components/Button/demo.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <ButtonGroup>
+      <Button>default</Button>
+      <Button disabled>default disabled</Button>
+      <Button type="primary">
+        <i class='fab fa-github-square' slot="icon"></i>
+        primary</Button>
+      <Button type="primary" disabled>primary disabled</Button>
+      <Button type="warn">warning</Button>
+      <Button type="warn" disabled>warning disabled</Button>
+      <Button type="primary" outlined>primary outlined</Button>
+      <Button type="primary" loading>loading</Button>
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button size="small" inline>small</Button>
+    </ButtonGroup>
+    <ButtonGroup inline>
+      <Button type="primary" >primary</Button>
+      <Button type="warn" >primary</Button>
+      <Button >primary</Button>
+    </ButtonGroup>
+    <ButtonGroup inline>
+      <Button type="primary" >primary</Button>
+      <Button type="warn" >primary</Button>
+      <Button >primary</Button>
+      <Button type="primary" >primary</Button>
+      <Button type="warn" >primary</Button>
+      <Button>primary</Button>
+    </ButtonGroup>
+
+    <ButtonGroup addons>
+      <Button type="primary" >primary</Button>
+      <Button type="warn" >primary</Button>
+      <Button type="primary">primary</Button>
+    </ButtonGroup>
+    <ButtonGroup sticky>
+      <Button type="primary" >primary</Button>
+      <Button type="warn" >primary</Button>
+    </ButtonGroup>
+    </div>
+
+</template>
+<script>
+import { Component, Vue } from "vue-property-decorator";
+import { Button, ButtonGroup } from './index.js'
+
+@Component({
+  components: {
+    Button,
+    ButtonGroup
+  }
+})
+export default class ButtonDemo extends Vue {}
+</script>
