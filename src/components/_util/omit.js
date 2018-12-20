5f95d31a3fae2d5a23b2e8425d7c8675995b0f3d
Evergreen Wang <vincentwang.720@gmail.com>
2018-12-20 20:24:27 +0800

    feat(Util): 新增 dom 操作帮助方法

---
+++ src/components/_util/omit.js
@@ -0,0 +1,13 @@
+//  https://github.com/benjycui/omit.js
+function omit(obj, fields) {
+  const shallowCopy = {
+    ...obj
+  };
+  for (let i = 0; i < fields.length; i++) {
+    const key = fields[i];
+    delete shallowCopy[key];
+  }
+  return shallowCopy;
+}
+
+export default omit;
