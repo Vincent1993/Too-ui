247d2d8124474f874676012217ed8071102ef37f
Evergreen Wang <vincentwang.720@gmail.com>
2019-01-11 18:31:17 +0800

    chore: 新增 postcss 配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+const path = require('path');
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    }
+  }
 };
