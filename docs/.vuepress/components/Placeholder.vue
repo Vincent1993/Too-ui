fa4a2167cb3852bc5711f14a12ac7d12a1513e72
Vincent Wang <vincentwang.720+fork@gmail.com>
2019-02-09 21:00:03 +0800

    docs(Placeholder): add placeholder component

---
+++ docs/.vuepress/components/Placeholder.vue
@@ -0,0 +1,31 @@
+<template>
+  <span :class="['placeholder', shape]" />
+</template>
+<script>
+export default {
+  props: {
+    shape: {
+      type: String,
+      default: 'round',
+    },
+  },
+};
+</script>
+<style lang="scss">
+.placeholder {
+  display: block;
+
+  width: 48px;
+  height: 48px;
+
+  background: #ddd;
+
+  &.round {
+    border-radius: 100%;
+  }
+
+  &.square {
+    border-radius: 5px;
+  }
+}
+</style>
